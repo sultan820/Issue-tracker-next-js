4936a50f11478a19aa768c6a3b1d0454a630bc48
Sultan Butt <sultanbutt820@gmail.com>
2023-11-24 04:01:13 +0500

    code refactor

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,7 +1,6 @@
 import React from "react";
-import { Flex, Card, Box } from "@radix-ui/themes";
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
+import { Skeleton } from "@/app/components";
+import { Box, Card, Flex } from "@radix-ui/themes";
 
 const LoadingIssueDetailPage = () => {
   return (
